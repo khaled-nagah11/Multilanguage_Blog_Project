4aced8da396fe11829237fb48485915ae45d04f2
khaled <kaled21@std.mans.edu.eg>
2024-03-31 22:19:53 +0200

    handlle dashboard layoutes in resource/view

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('dashboard.layouts.layout');
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
 });
