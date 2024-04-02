ebd6a5bf50241466702ab3f699b3ca5cad6416b6
khaled <kaled21@std.mans.edu.eg>
2024-04-02 19:29:39 +0200

    add setting request validation to validate the form

--- app/Http/Controllers/Dashboard/SettingController.php
+++ app/Http/Controllers/Dashboard/SettingController.php
@@ -3,30 +3,31 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SettingRequest;
 use App\Models\Setting;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 class SettingController extends Controller
 {
-    public function update(Request $request , Setting $setting)
+    public function update(SettingRequest $request , Setting $setting)
     {
-        $data = [
-            'image' => 'nullable|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
-            'favicon' => 'nullable|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
-            'facebook' => 'nullable|string',
-            'instagram' => 'nullable|string',
-            'phone' => 'nullable|string',
-            'email' => 'nullable|email',
+//        $data = [
+//            'image' => 'nullable|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
+//            'favicon' => 'nullable|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
+//            'facebook' => 'nullable|string',
+//            'instagram' => 'nullable|string',
+//            'phone' => 'nullable|string',
+//            'email' => 'nullable|email',
 
-        ];
+//        ];
         foreach (config('app.languages') as $key => $value) {
             $data[$key . '*.title'] = 'nullable|string';
             $data[$key . '*.content'] = 'nullable|string';
             $data[$key . '*.address'] = 'nullable|string';
         }
 
-        $validatedData = $request->validate($data);
+        $validatedData = $request->validate();
         $setting->update($request->except('image', 'favicon', '_token'));
 
         if ($request->file('logo'))
---
+++ app/Http/Requests/SettingRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SettingRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'image' => 'nullable|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
+            'favicon' => 'nullable|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
+            'facebook' => 'nullable|string',
+            'instagram' => 'nullable|string',
+            'phone' => 'required|string',
+            'email' => 'nullable|email',
+        ];
+    }
+}
