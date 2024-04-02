e7d71ef418bd942f17321b928620a66228b1e9dd
khaled <kaled21@std.mans.edu.eg>
2024-04-02 22:15:19 +0200

    add CheckLogin midlleware and define it in kernal

--- resources/views/dashboard/layouts/layout.blade.php
+++ resources/views/dashboard/layouts/layout.blade.php
@@ -77,6 +77,13 @@
                     <!--<a class="dropdown-item" href="#"><i class="fa fa-usd"></i> Payments<span class="tag tag-default">42</span></a>-->
                     <div class="divider"></div>
                     <a class="dropdown-item" href="#"><i class="fa fa-lock"></i> خروج</a>
+                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        {{ __('words.Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
                 </div>
             </li>
             <li class="nav-item">
